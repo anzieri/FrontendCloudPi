4dd36ce5dcac9f699070542bcfc1cbe8033a8e08
Amara Nyanzi <amaranyanzi1@gmail.com>
2024-12-07 23:29:21 +0300

    env change

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -56,7 +56,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e52a51c4a0d9c8662730d2a1f43933f1
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
-"flutter_bootstrap.js": "779cc6dc4292e4280ef4c8cd2c3a8ba8",
+"flutter_bootstrap.js": "693de8f7f5e2d83d1dc43bb260903138",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
